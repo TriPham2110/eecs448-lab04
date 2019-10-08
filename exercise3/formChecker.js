b967e013b08b71ec10cd24ea4e7f827d699a7c52
TriPham2110 <TriPham2110@users.noreply.github.com>
2019-10-08 14:23:52 -0500

    Update constraints for formChecker.js

--- exercise3/formChecker.js
+++ exercise3/formChecker.js
@@ -7,17 +7,43 @@ _7dayShipping = document.getElementById("free");
 _1dayShipping = document.getElementById("$50")
 _3dayShipping = document.getElementById("$5");
 
-//validateEmail is implemented using type="email" in customerFrontend.html
-
-function validateInput(){
-  if(!validatePassword(password))
-    alert("Invalid password");
-  if(!validateQuantity(tulips) || !validateQuantity(roses) || !validateQuantity(sunflowers))
-    alert("Invalid quantity input")
-  if(!validateShipOption(_7dayShipping) && !validateShipOption(_1dayShipping) && !validateShipOption(_3dayShipping))
-    alert("Invalid shipping option")
+function validateForm(){
+	if(!validateInput())
+		event.preventDefault();
+	else
+		return true;
 }
 
+function validateInput(){
+	var isValid = true;
+	if(!validateUsername(username)){
+		alert("Invalid username");
+		isValid = false;
+	}
+	if(!validatePassword(password)){
+		alert("Invalid password");
+		isValid = false;
+	}
+	if(!validateQuantity(tulips) || !validateQuantity(roses) || !validateQuantity(sunflowers)){
+		alert("Invalid quantity input");
+		isValid = false;
+	}
+	if(!validateShipOption(_7dayShipping) && !validateShipOption(_1dayShipping) && !validateShipOption(_3dayShipping)){
+		alert("Invalid shipping option");
+		isValid = false;
+	}
+	return isValid;
+}
+
+//validateUsername is further implemented using type="email" in customerFrontend.html
+function validateUsername(name){
+	if(name.value == "")
+		return false;
+	else
+		return true;
+}
+
+//validateQuantity is further implemented using min=0 in customerFrontend.html
 function validateQuantity(quantity){
   if(quantity.value < 0 || quantity.value =="")
     return false;
